022a5063e2e21fcb272f9def88ba4435d01d32df
codewithmunmun <munmunkum8210@gmail.com>
2026-05-17 00:24:23 +0530

    Updated README.md file and added tailwind-css folder

---
+++ 6_tailwind-css/src/App.jsx
@@ -0,0 +1,11 @@
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900'>
+      { /* h-screen: height: 100vh, w-full: width: 100%, bg-gray-900: background-color: #1a202c */}
+      App
+    </div>
+  )
+}
+
+export default App
